d2839a1d6575ab02c4be74106cc9d986a4277421
Zhenchao Li <cockneykevin@gmail.com>
2012-03-05 18:54:21 +0800

    haskell 71

---
+++ haskell/71.hs
@@ -0,0 +1,22 @@
+data Tree a = Node a [Tree a]
+              deriving (Eq, Show)
+
+tree1 = Node 'a' []
+
+tree2 = Node 'a' [Node 'b' []]
+
+tree3 = Node 'a' [Node 'b' [Node 'c' []]]
+
+tree4 = Node 'b' [Node 'd' [], Node 'e' []]
+
+tree5 = Node 'a' [
+                Node 'f' [Node 'g' []],
+                Node 'c' [],
+                Node 'b' [Node 'd' [], Node 'e' []]
+                ]
+
+ipl :: Tree a -> Int
+ipl t = iplAtLevel 1 t
+
+iplAtLevel :: Int -> Tree a -> Int
+iplAtLevel lvl (Node _ l) = (length l) * lvl + ((sum . map (iplAtLevel (lvl + 1))) l)
